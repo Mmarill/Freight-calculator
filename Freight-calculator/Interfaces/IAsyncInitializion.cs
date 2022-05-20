d7812825c07d741501a438e9c518e113cf420a47
AiDroopy <bjorn.milebjork@gmail.com>
2022-05-20 08:32:14 +0200

    Started with interface for async constructors

---
+++ Freight-calculator/Interfaces/IAsyncInitializion.cs
@@ -0,0 +1,42 @@
+﻿using Freight_calculator.Models;
+
+namespace Freight_calculator.Interfaces
+{
+    public interface IAsyncInitializion
+    {
+        public int Id { get; set; }
+       
+        public string? Address { get; set; }
+        public string City { get; set; }
+        public string? ZipCode { get; set; }
+        public string Country { get; set; }
+
+        public string AuctionId { get; set; }
+
+
+        
+
+        public double? DeliveryCost { get; set; }
+        public bool? Dilivered { get; set; }
+
+
+        public double? Tariff { get; set; }
+        public double? FixedCosts { get; set; }
+
+        public double? DistanceInKm { get; set; }
+        public Point2D? DestinationGPSPoint { get; set; }
+        public Point2D? AuctionGPSPoint { get; set; }
+        public bool VerboseMode { get; set; }
+        Task Initializaion { get; }
+
+         
+
+
+        private async Task<Object> InitializeAsync()
+        {
+            // asyncData = await GetDataAsync();
+            
+            return this;
+        }
+    }
+}
